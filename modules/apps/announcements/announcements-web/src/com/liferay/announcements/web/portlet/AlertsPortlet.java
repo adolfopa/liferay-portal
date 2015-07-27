32afaa79cae0bbd15077669829ad02f3f6c8f20c
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2015-07-27 19:08:50 +0200

    LPS-57393 This shouldn't be necessary since we are no longer using StrutsAction

--- modules/apps/announcements/announcements-web/src/com/liferay/announcements/web/portlet/AlertsPortlet.java
+++ modules/apps/announcements/announcements-web/src/com/liferay/announcements/web/portlet/AlertsPortlet.java
@@ -36,7 +36,6 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.private-request-attributes=false",
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
-		"com.liferay.portlet.struts-path=alerts",
 		"com.liferay.portlet.use-default-template=true",
 		"javax.portlet.display-name=Alerts", "javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.template-path=/",

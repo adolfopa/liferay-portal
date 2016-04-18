9cf78aa6ae6162ebf6ffe5958e5c4031531a6264
Adolfo Pérez <adolfo.perez@liferay.com>
2016-04-18 18:46:47 +0200

    LPS-64788 Run also on 7.0.1 upgrade

--- portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
+++ portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_1.java
@@ -21,6 +21,7 @@ import com.liferay.portal.upgrade.v7_0_1.UpgradeDocumentLibrary;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeMessageBoards;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeModules;
 import com.liferay.portal.upgrade.v7_0_1.UpgradeSchema;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeNotifications;
 
 /**
  * @author Brian Wing Shun Chan
@@ -40,6 +41,7 @@ public class UpgradeProcess_7_0_1 extends UpgradeProcess {
 		upgrade(UpgradeDocumentLibrary.class);
 		upgrade(UpgradeMessageBoards.class);
 		upgrade(UpgradeModules.class);
+		upgrade(UpgradeNotifications.class);
 
 		clearIndexesCache();
 	}

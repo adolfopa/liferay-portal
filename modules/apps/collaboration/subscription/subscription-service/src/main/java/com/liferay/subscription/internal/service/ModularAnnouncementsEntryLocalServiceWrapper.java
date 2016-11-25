e2cbe32056b9ce4a585a4c6f16e9a0201d0554f1
Adolfo Pérez <adolfo.perez@liferay.com>
2016-11-25 12:17:12 +0100

    LPS-69324 Decouple announcements from subscriptions

---
+++ modules/apps/collaboration/subscription/subscription-service/src/main/java/com/liferay/subscription/internal/service/ModularAnnouncementsEntryLocalServiceWrapper.java
@@ -0,0 +1,332 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.subscription.internal.service;
+
+import com.liferay.announcements.kernel.model.AnnouncementsDelivery;
+import com.liferay.announcements.kernel.model.AnnouncementsEntry;
+import com.liferay.announcements.kernel.service.AnnouncementsDeliveryLocalService;
+import com.liferay.announcements.kernel.service.AnnouncementsEntryLocalService;
+import com.liferay.announcements.kernel.service.AnnouncementsEntryLocalServiceWrapper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.interval.IntervalActionProcessor;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.SubscriptionSender;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.util.PrefsPropsUtil;
+import com.liferay.portal.util.PropsValues;
+import com.liferay.util.ContentUtil;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class ModularAnnouncementsEntryLocalServiceWrapper
+	extends AnnouncementsEntryLocalServiceWrapper {
+
+	public ModularAnnouncementsEntryLocalServiceWrapper() {
+		super(null);
+	}
+
+	public ModularAnnouncementsEntryLocalServiceWrapper(
+		AnnouncementsEntryLocalService announcementsEntryLocalService) {
+
+		super(announcementsEntryLocalService);
+	}
+
+	@Override
+	public void checkEntries() throws PortalException {
+		Date now = new Date();
+
+		if (_previousCheckDate == null) {
+			_previousCheckDate = new Date(
+				now.getTime() - _ANNOUNCEMENTS_ENTRY_CHECK_INTERVAL);
+		}
+
+		List<AnnouncementsEntry> entries =
+			announcementsEntryFinder.findByDisplayDate(now, _previousCheckDate);
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Processing " + entries.size() + " entries");
+		}
+
+		for (AnnouncementsEntry entry : entries) {
+			notifyUsers(entry);
+		}
+
+		_previousCheckDate = now;
+	}
+
+	protected void notifyUsers(AnnouncementsEntry entry)
+		throws PortalException {
+
+		Company company = companyPersistence.findByPrimaryKey(
+			entry.getCompanyId());
+
+		String className = entry.getClassName();
+		long classPK = entry.getClassPK();
+
+		String toName = PropsValues.ANNOUNCEMENTS_EMAIL_TO_NAME;
+		String toAddress = PropsValues.ANNOUNCEMENTS_EMAIL_TO_ADDRESS;
+
+		long teamId = 0;
+
+		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
+
+		params.put("announcementsDeliveryEmailOrSms", entry.getType());
+
+		if (classPK > 0) {
+			if (className.equals(Group.class.getName())) {
+				Group group = groupPersistence.findByPrimaryKey(classPK);
+
+				toName = group.getDescriptiveName();
+
+				params.put("inherit", Boolean.TRUE);
+				params.put("usersGroups", classPK);
+			}
+			else if (className.equals(Organization.class.getName())) {
+				Organization organization =
+					organizationPersistence.findByPrimaryKey(classPK);
+
+				toName = organization.getName();
+
+				params.put(
+					"usersOrgsTree",
+					ListUtil.fromArray(new Organization[] {organization}));
+			}
+			else if (className.equals(Role.class.getName())) {
+				Role role = rolePersistence.findByPrimaryKey(classPK);
+
+				toName = role.getName();
+
+				if (role.getType() == RoleConstants.TYPE_REGULAR) {
+					params.put("inherit", Boolean.TRUE);
+					params.put("usersRoles", classPK);
+				}
+				else if (role.isTeam()) {
+					teamId = role.getClassPK();
+				}
+				else {
+					params.put(
+						"userGroupRole", new Long[] {Long.valueOf(0), classPK});
+				}
+			}
+			else if (className.equals(UserGroup.class.getName())) {
+				UserGroup userGroup = userGroupPersistence.findByPrimaryKey(
+					classPK);
+
+				toName = userGroup.getName();
+
+				params.put("usersUserGroups", classPK);
+			}
+		}
+
+		if (className.equals(User.class.getName())) {
+			User user = userPersistence.findByPrimaryKey(classPK);
+
+			if (Validator.isNull(user.getEmailAddress())) {
+				return;
+			}
+
+			notifyUsers(
+				ListUtil.fromArray(new User[] {user}), entry,
+				company.getLocale(), user.getEmailAddress(),
+				user.getFullName());
+		}
+		else {
+			notifyUsers(entry, teamId, params, toName, toAddress, company);
+		}
+	}
+
+	protected void notifyUsers(
+			final AnnouncementsEntry entry, final long teamId,
+			final LinkedHashMap<String, Object> params, final String toName,
+			final String toAddress, final Company company)
+		throws PortalException {
+
+		int total = 0;
+
+		if (teamId > 0) {
+			total = userLocalService.getTeamUsersCount(teamId);
+		}
+		else {
+			total = userLocalService.searchCount(
+				company.getCompanyId(), null, WorkflowConstants.STATUS_APPROVED,
+				params);
+		}
+
+		final IntervalActionProcessor<Void> intervalActionProcessor =
+			new IntervalActionProcessor<>(total);
+
+		intervalActionProcessor.setPerformIntervalActionMethod(
+			new IntervalActionProcessor.PerformIntervalActionMethod<Void>() {
+
+				@Override
+				public Void performIntervalAction(int start, int end)
+					throws PortalException {
+
+					List<User> users = null;
+
+					if (teamId > 0) {
+						users = userLocalService.getTeamUsers(
+							teamId, start, end);
+					}
+					else {
+						users = userLocalService.search(
+							company.getCompanyId(), null,
+							WorkflowConstants.STATUS_APPROVED, params, start,
+							end, (OrderByComparator<User>)null);
+					}
+
+					notifyUsers(
+						users, entry, company.getLocale(), toAddress, toName);
+
+					intervalActionProcessor.incrementStart(users.size());
+
+					return null;
+				}
+
+			});
+
+		intervalActionProcessor.performIntervalActions();
+	}
+
+	protected void notifyUsers(
+			List<User> users, AnnouncementsEntry entry, Locale locale,
+			String toAddress, String toName)
+		throws PortalException {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("Notifying " + users.size() + " users");
+		}
+
+		boolean notifyUsers = false;
+
+		SubscriptionSender subscriptionSender = new SubscriptionSender();
+
+		for (User user : users) {
+			AnnouncementsDelivery announcementsDelivery =
+				announcementsDeliveryLocalService.getUserDelivery(
+					user.getUserId(), entry.getType());
+
+			if (announcementsDelivery.isEmail()) {
+				subscriptionSender.addRuntimeSubscribers(
+					user.getEmailAddress(), user.getFullName());
+
+				notifyUsers = true;
+			}
+
+			if (announcementsDelivery.isSms()) {
+				String smsSn = user.getContact().getSmsSn();
+
+				subscriptionSender.addRuntimeSubscribers(
+					smsSn, user.getFullName());
+
+				notifyUsers = true;
+			}
+		}
+
+		if (!notifyUsers) {
+			return;
+		}
+
+		String body = ContentUtil.get(PropsValues.ANNOUNCEMENTS_EMAIL_BODY);
+		String fromAddress = PrefsPropsUtil.getStringFromNames(
+			entry.getCompanyId(), PropsKeys.ANNOUNCEMENTS_EMAIL_FROM_ADDRESS,
+			PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
+		String fromName = PrefsPropsUtil.getStringFromNames(
+			entry.getCompanyId(), PropsKeys.ANNOUNCEMENTS_EMAIL_FROM_NAME,
+			PropsKeys.ADMIN_EMAIL_FROM_NAME);
+		String subject = ContentUtil.get(
+			PropsValues.ANNOUNCEMENTS_EMAIL_SUBJECT);
+
+		subscriptionSender.setBody(body);
+		subscriptionSender.setCompanyId(entry.getCompanyId());
+		subscriptionSender.setContextAttribute(
+			"[$ENTRY_CONTENT$]", entry.getContent(), false);
+		subscriptionSender.setContextAttributes(
+			"[$ENTRY_ID$]", entry.getEntryId(), "[$ENTRY_TITLE$]",
+			entry.getTitle(), "[$ENTRY_URL$]", entry.getUrl());
+		subscriptionSender.setFrom(fromAddress, fromName);
+		subscriptionSender.setHtmlFormat(true);
+		subscriptionSender.setLocalizedContextAttribute(
+			"[$ENTRY_TYPE$]",
+			notificationLocale ->
+				LanguageUtil.get(notificationLocale, entry.getType()));
+		subscriptionSender.setLocalizedContextAttribute(
+			"[$PORTLET_NAME$]",
+			notificationLocale -> LanguageUtil.get(
+				notificationLocale,
+				entry.isAlert() ? "alert" : "announcement"));
+		subscriptionSender.setMailId("announcements_entry", entry.getEntryId());
+
+		String portletId = PortletProviderUtil.getPortletId(
+			AnnouncementsEntry.class.getName(), PortletProvider.Action.VIEW);
+
+		subscriptionSender.setPortletId(portletId);
+
+		subscriptionSender.setScopeGroupId(entry.getGroupId());
+		subscriptionSender.setSubject(subject);
+
+		subscriptionSender.addRuntimeSubscribers(toAddress, toName);
+
+		subscriptionSender.flushNotificationsAsync();
+	}
+
+	@Reference(unbind = "-")
+	protected void setAnnouncementsDeliveryLocalService(
+		AnnouncementsDeliveryLocalService announcementsDeliveryLocalService) {
+
+		_announcementsDeliveryLocalService = announcementsDeliveryLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setUserLocalService(UserLocalService userLocalService) {
+		_userLocalService = userLocalService;
+	}
+
+	private static final long _ANNOUNCEMENTS_ENTRY_CHECK_INTERVAL =
+		PropsValues.ANNOUNCEMENTS_ENTRY_CHECK_INTERVAL * Time.MINUTE;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ModularAnnouncementsEntryLocalServiceWrapper.class);
+
+	private AnnouncementsDeliveryLocalService
+		_announcementsDeliveryLocalService;
+	private Date _previousCheckDate;
+	private UserLocalService _userLocalService;
+
+}

81dc6c372999c0b999183950c49e7f7dd53738ad
Roberto Díaz <roberto.diaz@liferay.com>
2018-09-26 16:22:41 +0200

    LPS-85419 Create a new base class to be implemented by the comments clients in order to update their classnames following their own subscription patterns.

---
+++ modules/apps/comment/comment-api/src/main/java/com/liferay/comment/upgrade/BaseUpgradeDiscussionSubscriptionClassName.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.comment.upgrade;
+
+import com.liferay.message.boards.model.MBDiscussion;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.subscription.model.Subscription;
+import com.liferay.subscription.service.SubscriptionLocalService;
+
+import java.util.List;
+
+/**
+ * @author Roberto Díaz
+ */
+public abstract class BaseUpgradeDiscussionSubscriptionClassName
+	extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		List<Subscription> subscriptions =
+			subscriptionLocalService.getSubscriptions(getClassName());
+
+		for (Subscription subscription : subscriptions) {
+			subscriptionLocalService.addSubscription(
+				subscription.getUserId(), subscription.getGroupId(),
+				MBDiscussion.class.getName() + StringPool.UNDERLINE +
+					getClassName(),
+				subscription.getClassPK());
+		}
+	}
+
+	protected abstract String getClassName();
+
+	protected SubscriptionLocalService subscriptionLocalService;
+
+}

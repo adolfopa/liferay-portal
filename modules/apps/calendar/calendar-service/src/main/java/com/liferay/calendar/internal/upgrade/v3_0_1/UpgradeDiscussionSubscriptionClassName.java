b558f5979ae7a3799de7025840bc66f1d944716b
Roberto Díaz <roberto.diaz@liferay.com>
2018-10-01 17:54:16 +0200

    LPS-85419 Implement the upgrade in calendar

---
+++ modules/apps/calendar/calendar-service/src/main/java/com/liferay/calendar/internal/upgrade/v3_0_1/UpgradeDiscussionSubscriptionClassName.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.calendar.internal.upgrade.v3_0_1;
+
+import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.comment.upgrade.BaseUpgradeDiscussionSubscriptionClassName;
+import com.liferay.subscription.model.Subscription;
+import com.liferay.subscription.service.SubscriptionLocalService;
+
+import java.util.List;
+
+/**
+ * @author Roberto Díaz
+ */
+public class UpgradeDiscussionSubscriptionClassName
+	extends BaseUpgradeDiscussionSubscriptionClassName {
+
+	public UpgradeDiscussionSubscriptionClassName(
+		SubscriptionLocalService service) {
+
+		subscriptionLocalService = service;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		super.doUpgrade();
+
+		List<Subscription> subscriptions =
+			subscriptionLocalService.getSubscriptions(getClassName());
+
+		for (Subscription subscription : subscriptions) {
+			subscriptionLocalService.deleteSubscription(
+				subscription.getSubscriptionId());
+		}
+	}
+
+	@Override
+	protected String getClassName() {
+		return CalendarBooking.class.getName();
+	}
+
+}

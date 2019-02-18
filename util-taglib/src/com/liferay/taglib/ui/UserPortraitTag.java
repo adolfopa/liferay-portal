d871420861002d82b4e316e114534ed206c78a43
Adolfo Pérez <adolfo.perez@liferay.com>
2019-02-18 09:30:56 +0100

    LPS-88324 Deprecate unused method

--- util-taglib/src/com/liferay/taglib/ui/UserPortraitTag.java
+++ util-taglib/src/com/liferay/taglib/ui/UserPortraitTag.java
@@ -86,8 +86,11 @@ public class UserPortraitTag extends IncludeTag {
 		_user = UserLocalServiceUtil.fetchUser(userId);
 	}
 
+	/**
+	 * @deprecated As of Mueller (7.2.x), with no direct replacement
+	 */
+	@Deprecated
 	public void setUserName(String userName) {
-		_userName = userName;
 	}
 
 	@Override
@@ -147,6 +150,5 @@ public class UserPortraitTag extends IncludeTag {
 
 	private String _cssClass = StringPool.BLANK;
 	private User _user;
-	private String _userName = StringPool.BLANK;
 
 }

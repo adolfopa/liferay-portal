231a4edf41c7123ad89ad1e17043dff45312141d
Alicia Garcia <alicia.garcia@liferay.com>
2019-03-13 18:35:56 +0100

    LPS-82637 api calls to both endpoints, entity and category

--- modules/apps/document-library/document-library-asset-auto-tagger-google-cloud-natural-language/src/main/java/com/liferay/document/library/asset/auto/tagger/google/cloud/natural/language/internal/GoogleCloudNaturalLanguageDocumentAssetAutoTagProvider.java
+++ modules/apps/document-library/document-library-asset-auto-tagger-google-cloud-natural-language/src/main/java/com/liferay/document/library/asset/auto/tagger/google/cloud/natural/language/internal/GoogleCloudNaturalLanguageDocumentAssetAutoTagProvider.java
@@ -66,9 +66,214 @@ public class GoogleCloudNaturalLanguageDocumentAssetAutoTagProvider
 	public List<String> getTagNames(FileEntry fileEntry) {
 		List<String> tagList = new ArrayList<>();
 
+		try {
+			GoogleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration
+				googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration =
+					_getConfiguration(fileEntry);
+
+			if (!_isTemporary(fileEntry) && _isSupportedFormat(fileEntry)) {
+				FileVersion fileVersion = fileEntry.getFileVersion();
+				Set<String> tags = new HashSet<>();
+
+				InputStream contentStream = fileVersion.getContentStream(false);
+
+				byte[] bytes = FileUtil.getBytes(contentStream);
+
+				String contentText = new String(bytes);
+
+				if (googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+						enabledClassification()) {
+
+					List<String> splitedTexts =
+						GoogleCloudNaturalLanguageUtil.splitTextToMaxSizeCall(
+							contentText,
+							GoogleCloudNaturalLanguageAssetAutoTagProviderConstants.MAX_CHARACTERS_SERVICE);
+
+					for (String payloadFragment : splitedTexts) {
+						JSONObject responseJSONObject =
+							_queryCloudNaturalLanguageJSONObject(
+								googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+									apiKey(),
+								payloadFragment, "classifyText");
+
+						JSONArray responsesJSONArray =
+							responseJSONObject.getJSONArray("categories");
+
+						if ((responsesJSONArray != null) &&
+							(responsesJSONArray.length() > 0)) {
+
+							for (int i = 0; i < responsesJSONArray.length();
+								 i++) {
+
+								JSONObject tagCandidate =
+									responsesJSONArray.getJSONObject(i);
+
+								String tag = tagCandidate.getString("name");
+
+								_clearDivideTags(tags, tag);
+							}
+
+							tagList.addAll(tags);
+						}
+					}
+				}
+
+				if (googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+						enabledEntity()) {
+
+					List<String> splitedTexts =
+						GoogleCloudNaturalLanguageUtil.splitTextToMaxSizeCall(
+							contentText,
+							GoogleCloudNaturalLanguageAssetAutoTagProviderConstants.MAX_CHARACTERS_SERVICE);
+
+					for (String payloadFragment : splitedTexts) {
+						JSONObject responseJSONObject =
+							_queryCloudNaturalLanguageJSONObject(
+								googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+									apiKey(),
+								payloadFragment, "analyzeEntities");
+
+						JSONArray responsesJSONArray =
+							responseJSONObject.getJSONArray("entities");
+
+						if ((responsesJSONArray != null) &&
+							(responsesJSONArray.length() > 0)) {
+
+							for (int i = 0; i < responsesJSONArray.length();
+								 i++) {
+
+								JSONObject tagCandidate =
+									responsesJSONArray.getJSONObject(i);
+
+								String salience = tagCandidate.getString(
+									"salience");
+
+								double salienceValue = 0D;
+
+								if (!salience.isEmpty()) {
+									salienceValue = Double.parseDouble(
+										salience);
+								}
+
+								if (salienceValue >
+										googleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+											salience()) {
+
+									String tag = tagCandidate.getString("name");
+
+									_clearDivideTags(tags, tag);
+								}
+							}
+						}
+
+						tagList.addAll(tags);
+					}
+				}
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
 
 		return tagList;
 	}
 
+	private void _clearDivideTags(Set<String> tags, String untratedTag) {
+		untratedTag.replaceAll(StringPool.DASH, StringPool.BLANK);
+		untratedTag.replaceAll(StringPool.APOSTROPHE, StringPool.BLANK);
+
+		if (!untratedTag.isEmpty()) {
+			String[] split = untratedTag.split(StringPool.AMPERSAND, -1);
+
+			List<String> candidates = Stream.of(
+				split
+			).flatMap(
+				elem -> Stream.of(elem.split(StringPool.FORWARD_SLASH, 0))
+			).filter(
+				tagCandidate -> !tagCandidate.isEmpty()
+			).map(
+				String::trim
+			).collect(
+				Collectors.toList()
+			);
+
+			tags.addAll(candidates);
+		}
+	}
+
+	private GoogleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration
+			_getConfiguration(FileEntry fileEntry)
+		throws ConfigurationException {
+
+		return _configurationProvider.getConfiguration(
+			GoogleCloudNaturalLanguageAssetAutoTagProviderCompanyConfiguration.
+				class,
+			new CompanyServiceSettingsLocator(
+				fileEntry.getCompanyId(),
+				GoogleCloudNaturalLanguageAssetAutoTagProviderConstants.
+					SERVICE_NAME));
+	}
+
+	private boolean _isSupportedFormat(FileEntry fileEntry) {
+		String extension = fileEntry.getExtension();
+
+		return _supportedFormats.contains(StringUtil.toUpperCase(extension));
+	}
+
+	private boolean _isTemporary(FileEntry fileEntry) {
+		return fileEntry.isRepositoryCapabilityProvided(
+			TemporaryFileEntriesCapability.class);
+	}
+
+	private JSONObject _queryCloudNaturalLanguageJSONObject(
+			String apiKey, String payloadJSON, String endpoint)
+		throws Exception {
+
+		Http.Options options = new Http.Options();
+
+		options.addHeader("Content-Type", ContentTypes.APPLICATION_JSON);
+		options.setBody(
+			payloadJSON, ContentTypes.APPLICATION_JSON, StringPool.UTF8);
+		options.setLocation(
+			StringBundler.concat(
+				"https://language.googleapis.com/v1/documents:", endpoint,
+				"?key=", apiKey));
+		options.setPost(true);
+
+		String responseJSON = _http.URLtoString(options);
+
+		Http.Response response = options.getResponse();
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(responseJSON);
+
+		if (response.getResponseCode() != HttpURLConnection.HTTP_OK) {
+			JSONObject errorJSONObject = jsonObject.getJSONObject("error");
+
+			String errorMessage = responseJSON;
+
+			if (errorJSONObject != null) {
+				errorMessage = errorJSONObject.getString("message");
+			}
+
+			throw new PortalException(
+				StringBundler.concat(
+					"Response code ", response.getResponseCode(), ": ",
+					errorMessage));
+		}
+
+		return jsonObject;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GoogleCloudNaturalLanguageDocumentAssetAutoTagProvider.class);
+
+	private static final Set<String> _supportedFormats = new HashSet<>(
+		Arrays.asList("TXT"));
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private Http _http;
 
 }

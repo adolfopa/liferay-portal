5fb0c65e0214c81814b722e3d5d31e0deecd9714
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2023-04-14 10:59:21 +0200

    LPS-180298 Add storage area to resolve blob paths

---
+++ portal-kernel/src/com/liferay/document/library/kernel/store/StoreArea.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.kernel.store;
+
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.petra.lang.CentralizedThreadLocal;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Adolfo Pérez
+ */
+public enum StoreArea {
+
+	DELETED("_deleted"), EVICTED("_evicted"), LIVE(StringPool.BLANK);
+
+	public static String getPath(String... path) {
+		StoreArea storeArea = _storeAreaThreadLocal.get();
+
+		return storeArea._namespace + StringPool.SLASH +
+			StringUtil.merge(path, StringPool.SLASH);
+	}
+
+	public static <T extends Throwable> void withStoreArea(
+			StoreArea readStoreArea, UnsafeRunnable<T> unsafeRunnable)
+		throws T {
+
+		StoreArea oldStoreArea = _storeAreaThreadLocal.get();
+
+		try {
+			_storeAreaThreadLocal.set(readStoreArea);
+
+			unsafeRunnable.run();
+		}
+		finally {
+			_storeAreaThreadLocal.set(oldStoreArea);
+		}
+	}
+
+	private StoreArea(String namespace) {
+		_namespace = namespace;
+	}
+
+	private static final ThreadLocal<StoreArea> _storeAreaThreadLocal =
+		new CentralizedThreadLocal<>(
+			StoreArea.class.getName() + "._readStoreAreaThreadLocal",
+			() -> LIVE);
+
+	private final String _namespace;
+
+}

1939b986eabb584a4f90188002b2beb907604a45
Roberto Díaz <roberto.diaz@liferay.com>
2025-03-19 16:14:46 +0100

    LPD-49536 test

--- modules/apps/headless/headless-asset-library/headless-asset-library-test/src/testIntegration/java/com/liferay/headless/asset/library/resource/v1_0/test/UserGroupResourceTest.java
+++ modules/apps/headless/headless-asset-library/headless-asset-library-test/src/testIntegration/java/com/liferay/headless/asset/library/resource/v1_0/test/UserGroupResourceTest.java
@@ -7,13 +7,63 @@ package com.liferay.headless.asset.library.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 
+import com.liferay.depot.model.DepotEntry;
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.headless.asset.library.client.dto.v1_0.UserGroup;
+import com.liferay.headless.asset.library.client.resource.v1_0.AssetLibraryResource;
+import com.liferay.headless.asset.library.dto.v1_0.AssetLibrary;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.FeatureFlags;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.util.PropsValues;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Roberto Díaz
  */
-@Ignore
+@FeatureFlags("LPD-17564")
 @RunWith(Arquillian.class)
 public class UserGroupResourceTest extends BaseUserGroupResourceTestCase {
+
+	@Override
+	protected UserGroup testPostAssetLibraryByExternalReferenceCodeUserGroup_addUserGroup(
+		UserGroup userGroup) throws Exception {
+
+		com.liferay.portal.kernel.model.UserGroup userGroup1 =
+			_userGroupLocalService.addUserGroup(
+				userGroup.getExternalReferenceCode(),
+				TestPropsValues.getUserId(), TestPropsValues.getCompanyId(),
+				userGroup.getName(), RandomTestUtil.randomString(),
+				ServiceContextTestUtil.getServiceContext());
+
+		userGroup.setId(userGroup1.getUserGroupId());
+
+		userGroupResource.postAssetLibraryUserGroup(
+			testDepotEntry.getGroupId(), userGroup);
+
+		return userGroup;
+
+	}
+
+	@Override
+	public void testPostAssetLibraryUserGroup() throws Exception {
+		super.testPostAssetLibraryUserGroup();
+	}
+
+	@Inject
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Inject
+	private UserGroupLocalService _userGroupLocalService;
 }

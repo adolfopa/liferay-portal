dac2b1501c81e0a827d533c8ec93b09972a6a36c
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-05-01 15:42:40 +0200

    LPD-88227 Convert Section family (Contents, Files, Folder) to System FDS by registering ViewContentsSection, ViewFilesSection, ViewContentsFolder and ViewFilesFolder SystemFDSEntry @Components plus matching FDSItemsActions @Components. ViewFolderSectionDisplayContext already dispatches between contents and files variants based on root folder ERC, so each folder variant gets its own SystemFDSEntry trio. The FDSItemsActions @Components delegate to SectionDisplayContextHelper.get{Contents,Files}FDSActionDropdownItems (already centralized by LPD-88211) so the action lists stay in one place.

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/ViewFilesSectionSystemFDSEntry.java
@@ -0,0 +1,130 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set;
+
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.frontend.data.set.SystemFDSEntry;
+import com.liferay.object.model.ObjectEntryFolder;
+import com.liferay.object.service.ObjectDefinitionSettingLocalService;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+import com.liferay.site.cms.site.initializer.internal.display.context.SectionDisplayContextHelper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMSSiteInitializerFDSNames.FILES_SECTION,
+	service = SystemFDSEntry.class
+)
+public class ViewFilesSectionSystemFDSEntry implements SystemFDSEntry {
+
+	@Override
+	public String getAdditionalAPIURLParameters(
+		HttpServletRequest httpServletRequest) {
+
+		return _sectionDisplayContextHelper.getAdditionalAPIURLParameters(
+			_sectionDisplayContextHelper.appendStatus(
+				_sectionDisplayContextHelper.appendGroupIds(
+					"cmsRoot eq true and cmsSection eq 'files'",
+					httpServletRequest)),
+			httpServletRequest, null);
+	}
+
+	@Override
+	public int getDefaultItemsPerPage() {
+		return 20;
+	}
+
+	@Override
+	public String getDescription() {
+		return "CMS Files Section";
+	}
+
+	@Override
+	public boolean getHideManagementBarInEmptyState() {
+		return true;
+	}
+
+	@Override
+	public String getName() {
+		return CMSSiteInitializerFDSNames.FILES_SECTION;
+	}
+
+	@Override
+	public String getPropsTransformer() {
+		return "{AssetsFilesDropFDSPropsTransformer} from " +
+			"site-cms-site-initializer";
+	}
+
+	@Override
+	public String getRESTApplication() {
+		return "/search/v1.0";
+	}
+
+	@Override
+	public String getRESTEndpoint() {
+		return "/v1.0/search";
+	}
+
+	@Override
+	public String getRESTSchema() {
+		return "SearchResult";
+	}
+
+	@Override
+	public String getSymbol() {
+		return "documents-and-media";
+	}
+
+	@Override
+	public String getTitle() {
+		return "Files Section";
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_sectionDisplayContextHelper = new SectionDisplayContextHelper(
+			_depotEntryLocalService, _groupLocalService, _language,
+			_objectDefinitionSettingLocalService,
+			_objectEntryFolderModelResourcePermission, _portal);
+	}
+
+	@Reference
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private ObjectDefinitionSettingLocalService
+		_objectDefinitionSettingLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.object.model.ObjectEntryFolder)"
+	)
+	private ModelResourcePermission<ObjectEntryFolder>
+		_objectEntryFolderModelResourcePermission;
+
+	@Reference
+	private Portal _portal;
+
+	private SectionDisplayContextHelper _sectionDisplayContextHelper;
+
+}
---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/action/ViewFilesFolderFDSItemsActions.java
@@ -0,0 +1,78 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set.action;
+
+import com.liferay.depot.service.DepotEntryLocalService;
+import com.liferay.frontend.data.set.action.FDSItemsActions;
+import com.liferay.frontend.data.set.model.FDSActionDropdownItem;
+import com.liferay.object.model.ObjectEntryFolder;
+import com.liferay.object.service.ObjectDefinitionSettingLocalService;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+import com.liferay.site.cms.site.initializer.internal.display.context.SectionDisplayContextHelper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMSSiteInitializerFDSNames.VIEW_FILES_FOLDER,
+	service = FDSItemsActions.class
+)
+public class ViewFilesFolderFDSItemsActions implements FDSItemsActions {
+
+	@Override
+	public List<FDSActionDropdownItem> getFDSActionDropdownItems(
+		HttpServletRequest httpServletRequest) {
+
+		return _sectionDisplayContextHelper.getFilesFDSActionDropdownItems(
+			httpServletRequest);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_sectionDisplayContextHelper = new SectionDisplayContextHelper(
+			_depotEntryLocalService, _groupLocalService, _language,
+			_objectDefinitionSettingLocalService,
+			_objectEntryFolderModelResourcePermission, _portal);
+	}
+
+	@Reference
+	private DepotEntryLocalService _depotEntryLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private ObjectDefinitionSettingLocalService
+		_objectDefinitionSettingLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.object.model.ObjectEntryFolder)"
+	)
+	private ModelResourcePermission<ObjectEntryFolder>
+		_objectEntryFolderModelResourcePermission;
+
+	@Reference
+	private Portal _portal;
+
+	private SectionDisplayContextHelper _sectionDisplayContextHelper;
+
+}

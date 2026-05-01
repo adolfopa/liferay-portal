a7a0d8eff4e4a656e5d3ed3861a786ae1bd5f47f
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2026-05-01 15:36:38 +0200

    LPD-88227 Convert Related Assets and All Related Assets sections to System FDS by registering ViewRelatedAssetsSectionSystemFDSEntry on RELATED_ASSETS_SECTION and ViewAllRelatedAssetsSectionSystemFDSEntry on ALL_RELATED_ASSETS_SECTION; display contexts and JSPs keep their existing wiring.

---
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/frontend/data/set/ViewAllRelatedAssetsSectionSystemFDSEntry.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.internal.frontend.data.set;
+
+import com.liferay.frontend.data.set.SystemFDSEntry;
+import com.liferay.site.cms.site.initializer.internal.constants.CMSSiteInitializerFDSNames;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Adolfo Pérez
+ */
+@Component(
+	property = "frontend.data.set.name=" + CMSSiteInitializerFDSNames.ALL_RELATED_ASSETS_SECTION,
+	service = SystemFDSEntry.class
+)
+public class ViewAllRelatedAssetsSectionSystemFDSEntry
+	implements SystemFDSEntry {
+
+	@Override
+	public String getDescription() {
+		return "CMS All Related Assets Section";
+	}
+
+	@Override
+	public String getName() {
+		return CMSSiteInitializerFDSNames.ALL_RELATED_ASSETS_SECTION;
+	}
+
+	@Override
+	public String getPropsTransformer() {
+		return "{AssetsFDSPropsTransformer} from site-cms-site-initializer";
+	}
+
+	@Override
+	public String getRESTApplication() {
+		return "/search/v1.0";
+	}
+
+	@Override
+	public String getRESTEndpoint() {
+		return "/v1.0/search";
+	}
+
+	@Override
+	public String getRESTSchema() {
+		return "SearchResult";
+	}
+
+	@Override
+	public String getSymbol() {
+		return "list-ul";
+	}
+
+	@Override
+	public String getTitle() {
+		return "All Related Assets Section";
+	}
+
+}
